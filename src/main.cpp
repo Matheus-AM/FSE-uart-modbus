f7c944f5d5ff7220f90950e8d94053b54145a0d2
Matheus-AM <mam.macacod@gmail.com>
2023-01-19 01:15:10 -0300

    add forno

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,71 @@
 #include <uartController.h>
 
-int main(int argc, const char * argv[]) {
-    if(argc != 3){
-        printf("\n$ ./exec <type> <msg>\n\ntype: 1(int), 2(float), 3(string)\nmsg: 00(recv), <text>");
+class Forno
+{
+private:
+    float temp_ambiente;
+    float temp_self;
+    float temp_ref;
+    uchar power;
+    uchar dash;
+    uchar play;
+    const static uchar POWER_ON_CODE = 0xA1;
+    const static uchar POWER_OFF_CODE = 0xA2;
+    const static uchar PLAY_CODE = 0xA3;
+    const static uchar CANCEL_CODE = 0xA4;
+    const static uchar MENU_CODE = 0xA5;
+
+public:
+    Forno(/* args */);
+    //
+    void handleUserCmd(int user_cmd);
+    ~Forno();
+};
+
+Forno::Forno(/* args */)
+{
+}
+
+Forno::~Forno()
+{
+}
+
+void Forno::handleUserCmd(int user_cmd){
+    switch (user_cmd)
+    {
+    case POWER_ON_CODE:
+        power = 1;
+        break;
+    case POWER_OFF_CODE:
+        power = 0;
+        break;
+    case PLAY_CODE:
+        play = 1;
+        break;
+    case CANCEL_CODE:
+        play = 1;
+    case MENU_CODE:
+        dash = !dash;
+        break;
+    default:
+        printf("user_cmd erro\n");
+        break;
     }
+}
+
+
+int main(int argc, const char * argv[]) {
     uchar matricula[4] = {0x00, 0x03, 0x00, 0x07};
     UartController uart(matricula);
-    // int cmd = handle_interface(argv[1], argv[2]);
-    // if(cmd > 0xa3){
-    uart.send_tx(0xc3, argv[2]);
-    // }else{
-    // uart.recv_rx(cmd);
-    // }
+    Forno forno();
+    while (1)
+    {
+        int user_cmd = uart.send_tx(0xc3, argv[2]);
+        if (user_cmd != -1) handleUserCmd(user_cmd);   
+        
+        usleep(500000);
+    }
+    
     uart.close_it();
     return 0;
 }
