27d824f71df1524ddc8bc9e7ec444fafb45f5918
Matheus-AM <mam.macacod@gmail.com>
2023-01-21 14:35:52 -0300

    tring to fic operato gpio

--- inc/gpio.h
+++ inc/gpio.h
@@ -11,5 +11,18 @@ private:
     int intensity = 0;
 public:
     GpioPWM(void);
-    void operator<<(const int intensity_);
+    void operator<<(const int intensity_){
+        if (intensity_ < 0){
+            if(intensity == -intensity_) return;
+            intensity = (-intensity_ < RANGE ? -intensity_: RANGE);
+            intensity = (-intensity_ > 40 ? -intensity_: 40);
+            softPwmWrite(fanPin, intensity); /* change the value of PWM */
+            softPwmWrite(resistPin, 0); /* change the value of PWM */
+        }else{
+            if(intensity == intensity_) return;
+            intensity = (intensity_ < RANGE ? intensity_: RANGE);
+            softPwmWrite(fanPin, 0); /* change the value of PWM */
+            softPwmWrite(resistPin, intensity);
+        }
+    };
 };
--- src/gpio.cpp
+++ src/gpio.cpp
@@ -9,17 +9,3 @@ GpioPWM::GpioPWM()
     pinMode(resistPin, OUTPUT);       /* set GPIO as output */
     softPwmCreate(resistPin, 0, RANGE); /* set PWM channel along with range*/
 }
-void GpioPWM::operator<<(const int intensity_){
-    if (intensity_ < 0){
-        if(intensity == -intensity_) return;
-        intensity = (-intensity_ < RANGE ? -intensity_: RANGE);
-        intensity = (-intensity_ > 40 ? -intensity_: 40);
-        softPwmWrite(fanPin, intensity); /* change the value of PWM */
-        softPwmWrite(resistPin, 0); /* change the value of PWM */
-    }else{
-        if(intensity == intensity_) return;
-        intensity = (intensity_ < RANGE ? intensity_: RANGE);
-        softPwmWrite(fanPin, 0); /* change the value of PWM */
-        softPwmWrite(resistPin, intensity);
-    }
-}
