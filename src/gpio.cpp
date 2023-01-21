408c2a24239c4765b5e88cd5663f3b03f2304033
Matheus-AM <mam.macacod@gmail.com>
2023-01-21 14:38:40 -0300

    fix operator gpio

--- src/gpio.cpp
+++ src/gpio.cpp
@@ -3,9 +3,30 @@
 
 GpioPWM::GpioPWM()
 {
-    wiringPiSetup();                /* initialize wiringPi setup */
-    pinMode(fanPin, OUTPUT);       /* set GPIO as output */
-    softPwmCreate(fanPin, 0, RANGE); /* set PWM channel along with range*/
-    pinMode(resistPin, OUTPUT);       /* set GPIO as output */
+    wiringPiSetup();                    /* initialize wiringPi setup */
+    pinMode(fanPin, OUTPUT);            /* set GPIO as output */
+    softPwmCreate(fanPin, 0, RANGE);    /* set PWM channel along with range*/
+    pinMode(resistPin, OUTPUT);         /* set GPIO as output */
     softPwmCreate(resistPin, 0, RANGE); /* set PWM channel along with range*/
 }
+
+GpioPWM::operator<<(const int intensity_)
+{
+    if (intensity_ < 0)
+    {
+        if (intensity == -intensity_)
+            return;
+        intensity = (-intensity_ < RANGE ? -intensity_ : RANGE);
+        intensity = (-intensity_ > 40 ? -intensity_ : 40);
+        softPwmWrite(fanPin, intensity); /* change the value of PWM */
+        softPwmWrite(resistPin, 0);      /* change the value of PWM */
+    }
+    else
+    {
+        if (intensity == intensity_)
+            return;
+        intensity = (intensity_ < RANGE ? intensity_ : RANGE);
+        softPwmWrite(fanPin, 0); /* change the value of PWM */
+        softPwmWrite(resistPin, intensity);
+    }
+};
