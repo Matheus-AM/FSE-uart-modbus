619c979319d9cec31e97083d004182a37e72e848
Matheus-AM <mam.macacod@gmail.com>
2023-01-23 17:51:58 -0300

    fix curva

--- src/csv_reader.cpp
+++ src/csv_reader.cpp
@@ -7,7 +7,14 @@ void read_record(struct csv* row )
   
     // Open an existing file
     fin.open("../curva_reflow.csv", std::ios::in);
-  
+    int a[10] = {25, 38, 46, 54, 57, 61, 63, 54, 33, 25};
+    if(!fin.is_open()){
+        for(int i =0; i<10;i++){
+            (*row).time[i] = a[i];
+            (*row).temp[i] = a[i];
+        }
+        return;
+    }
     
     // Read the Data from the file
     // as String Vector
@@ -22,7 +29,6 @@ void read_record(struct csv* row )
         (*row).time[count] = std::stoi(word);
         std::getline(s, word, ',');
         (*row).temp[count] = std::stoi(word);
-        printf("a %d ", (*row).time[count]);
         count++;
 
     }
